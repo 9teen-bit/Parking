1bfc9b7352d9ef79c16f769f9e960ebdbba3f2a0
9teen-bit <romainishimwe4@gmail.com>
2025-05-23 15:36:34 +0200

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '@elyssa123',
+  password: '',
   database: 'pssms',
   waitForConnections: true,
   connectionLimit: 10,
